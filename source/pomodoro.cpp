3f7680201eda7d6293066841bf95d7c81e8b5029
Chris <109439965+karydesc@users.noreply.github.com>
2023-11-10 14:45:24 +0200

    Update pomodoro.cpp

--- source/pomodoro.cpp
+++ source/pomodoro.cpp
@@ -24,15 +24,14 @@ void pomodoro::startSession(int workminutes,int breakminutes,wxStaticText* text,
 
 //restart pomodoro indefinitely
     while(true){
-        secs = workminutes * 60;
-        int j;
+        secs = workminutes * 60;//set loop range
         wxGetApp().CallAfter([gauge,secs](){ //set the range of the gauge
             gauge->SetRange(secs);
         });
     for (int i = secs; i >= 0;) {
-        if (this->cancelFlag) return;
+        if (this->cancelFlag) return;//check if user has requested to cancel
         if (quitRequested) return; //handling window close/end thread
-        if (!this->pauseflag) {
+        if (!this->pauseflag) { //the timer will tick and update the GUI ONLY if the flag is not set to true
             session->WorkSeconds++;
             wxGetApp().CallAfter([i, text, gauge]() {
                 text->SetLabelText(wxString::Format("Focus: %d:%02d", i / 60, i % 60)); //updating the timer
@@ -68,7 +67,7 @@ void pomodoro::pauseSession() {
     this->pauseflag= !pauseflag;
 }
 void pomodoro::resetScreen(wxStaticText* text, wxGauge* gauge){
-    wxGetApp().CallAfter([text,gauge]() {
+    wxGetApp().CallAfter([text,gauge]() { //clear the screen
         text->SetLabelText("Press start to initiate a session");
         gauge->SetValue(0);
     });
